661e1d12b327cb02a08e359b888cdde970dab4ca
MisaOgura <misa.ogura01@gmail.com>
2018-06-02 09:57:18 +0100

    Make base url constant

--- news-api/custom_query.py
+++ news-api/custom_query.py
@@ -13,36 +13,50 @@ load_dotenv(dotenv_path=env_path)
 # Load the value of API_KEY
 API_KEY = os.getenv('API_KEY')
 
-# Construct base_url and parameters to send
-base_url = 'https://newsapi.org/v2/everything'
-params = {
-    'q': 'pay gap',
-    'apiKey': API_KEY
-}
+# Set some constants
+PAGE_SIZE = 100
+BASE_URL = 'https://newsapi.org/v2/everything'
 
-# Get data from News API with the parameters set above
-response = requests.get(base_url, params=params)
+def run():
+    # Construct parameters to send
+    params = {
+        'q': 'gender pay gap',
+        'apiKey': API_KEY
+    }
 
-# Extract the JSON representation of the response data
-data = response.json()
-print(len(data['articles']), 'articles found!')
+    # Get the first set of data from News API with the parameters set above
+    print('\nGrabbing articles...')
+    response = requests.get(BASE_URL, params=params)
 
-# Save the JSON representation of the response data into a file
-with open('data.txt', 'w') as file:
-    json.dump(data, file, indent=4)
+    # Printing status for debugging
+    print('Received a response with status code: ', response.status_code)
 
-# Load the JSON data
-with open('data.txt','r') as file:
-    data = json.load(file)
+    # Extract the JSON representation of the response data
+    data = response.json()
+    print('\nTotal number of articles obtained: ', len(data['articles']))
 
-# Access information within the data - showing first 5 for demo purposes
-print('Displaying information on the first 5 articles...')
-for article in data['articles'][:5]:
-    published_datetime = dateutil.parser.parse(article['publishedAt'])
+    # Save the JSON representation of the response data into a file
+    print('\nSaving the data into a file...')
+    with open('data.txt', 'w') as file:
+        json.dump(data, file, indent=4)
 
-    print('\n')
-    print('Source: ', article['source']['name'])
-    print('Title: ', article['title'])
-    print('Written by: ', article['author'])
-    print('Published at:', published_datetime)
-    print('URL: ', article['url'])
+    # Load the JSON data
+    print('\nLoading data...')
+    with open('data.txt','r') as file:
+        data = json.load(file)
+    print('\nLoaded ', len(data['articles']), ' articles')
+
+    # Access information within the data - showing first 5 for demo purposes
+    print('\nDisplaying information of the first 5 articles...')
+    for article in data['articles'][:5]:
+        published_datetime = dateutil.parser.parse(article['publishedAt'])
+
+        print('\n')
+        print('Source: ', article['source']['name'])
+        print('Title: ', article['title'])
+        print('Written by: ', article['author'])
+        print('Published at:', published_datetime)
+        print('URL: ', article['url'])
+
+if __name__ == '__main__':
+    run()
