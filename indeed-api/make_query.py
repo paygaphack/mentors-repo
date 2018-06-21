1f6300b2bcc8ae256bd1e0a24711afe835b034c1
MisaOgura <misa.ogura01@gmail.com>
2018-06-21 21:42:54 +0100

    Prototype indeed api client

--- indeed-api/make_query.py
+++ indeed-api/make_query.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+from time import sleep
 
 from dotenv import load_dotenv
 from pathlib import Path
@@ -9,22 +10,27 @@ from pathlib import Path
 env_path = Path('./') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-# Load the value of API_KEY
-API_KEY = os.getenv('API_KEY')
+# Load the value of PUBLISHER_ID
+PUBLISHER_ID = os.getenv('PUBLISHER_ID')
 
 # Set some constants
-PAGE_SIZE = 100
-BASE_URL = 'https://newsapi.org/v2/everything'
+PAGE_SIZE = 25
+BASE_URL = 'http://api.indeed.com/ads/apisearch'
 
 def run():
-    # Construct parameters to send
+    # Construct parameters to send, pagination starts from 0
+    current_page = 0
     params = {
-        'q': 'gender pay gap',
-        'language': 'en',
-        'apiKey': API_KEY,
-        'sortBy': 'publishedAt',
-        'pageSize': PAGE_SIZE,
-        'page': 1,
+        'publisher': PUBLISHER_ID,
+        'v': 2,
+        'userip': '1.2.3.4',
+        'useragent': 'Mozilla/%2F4.0%28Firefox%29',
+        'q': '*',
+        'sort': 'date',
+        'format': 'json',
+        'co': 'gb',
+        'start': 0,
+        'limit': PAGE_SIZE
     }
 
     # Get the first set of data from News API with the parameters set above
@@ -42,12 +48,11 @@ def run():
     total_pages = calculate_total_pages(PAGE_SIZE, total_results)
     print('Total number of pages to paginate: ', total_pages, '\n')
 
-    # Grab the rest of the articles by paginating 100 articles at a time
-    current_page = 1
+    # Grab the rest of the articles by paginating 25 results at a time
     while current_page < total_pages:
         # Increment current_page and set it to the page param
         current_page += 1
-        params['page'] = current_page
+        params['start'] = current_page * 25
 
         # Obtain next page from the API
         print('\nGrabbing page: ', current_page, '...')
@@ -58,27 +63,27 @@ def run():
 
         # Breaks out from the while loop if the request fails
         new_data = response.json()
-        if 'articles' not in new_data:
+        if 'results' not in new_data:
             break
 
         # Merge new articles to the obtained list of articles
-        new_articles = new_data['articles']
-        data['articles'].extend(new_articles)
+        new_articles = new_data['results']
+        data['results'].extend(new_articles)
 
-    print('\nTotal number of articles obtained: ', len(data['articles']))
+    print('\nTotal number of articles obtained: ', len(data['results']))
 
     # Save the data into a file
     print('\nSaving the data into a file...')
-    with open('news_api_data.txt', 'w') as file:
+    with open('example.txt', 'w') as file:
         json.dump(data, file, indent=4)
-    print('\nData saved as news_api_data.txt!')
+    print('\nData saved as example.txt!')
 
 
 def calculate_total_pages(page_size, total_results):
     if total_results <= 100:
         return 1
     elif total_results % PAGE_SIZE == 0:
-        return total_results / PAGE_SIZE
+        return int(total_results / PAGE_SIZE)
     else:
         return int(total_results / PAGE_SIZE) + 1
 
