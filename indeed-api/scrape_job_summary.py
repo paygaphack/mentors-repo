6dd09b37a85f7d0bd4dded219e1b327851aa80cb
MisaOgura <misa.ogura01@gmail.com>
2018-06-22 11:43:22 +0100

    Run if the name is main

--- indeed-api/scrape_job_summary.py
+++ indeed-api/scrape_job_summary.py
@@ -2,20 +2,25 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-print('\nLoading data...')
+def run():
+    print('\nLoading data...')
 
-# Load JSON data
-with open('results.txt','r') as file:
-    data = json.load(file)
+    # Load JSON data
+    with open('results.txt','r') as file:
+        data = json.load(file)
 
-for i, job in enumerate(data['results']):
-    print('\nScraping a job summary for job no. ', i+1)
+    for i, job in enumerate(data['results']):
+        print('\nScraping a job summary for job no. ', i+1)
 
-    res = requests.get(job['url'])
-    soup = BeautifulSoup(res.text,'lxml')
-    summary = soup.find(id='job_summary')
-    job['jobsummary'] = str(summary)
+        res = requests.get(job['url'])
+        soup = BeautifulSoup(res.text,'lxml')
+        summary = soup.find(id='job_summary')
+        job['jobsummary'] = str(summary)
 
-with open('results_with_summary.txt', 'w') as file:
-    json.dump(data, file, indent=4)
-print('\nData saved as results_with_summary.txt!')
+    with open('results_with_summary.txt', 'w') as file:
+        json.dump(data, file, indent=4)
+    print('\nData saved as results_with_summary.txt!')
+
+
+if __name__ == '__main__':
+    run()
